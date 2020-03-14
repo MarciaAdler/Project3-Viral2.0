32579bdffd80f662d77ade9ce6759efc30c70a4e
Frank Moran <info@francisfrankmoran.com>
2020-03-13 20:59:10 -0400

    login page

--- client/src/pages/LoginLayout.js
+++ client/src/pages/LoginLayout.js
@@ -6,7 +6,7 @@ function LoginLayout(props) {
   
   return (
       <div className="h-100 w-100">
-          <Container fluid className="h-100 w-100 home-hero">
+          <Container fluid className="h-100 w-100 login-hero">
                 <Row className="h-100 justify-content-center">
                     <Col md={"auto"} className="mt-5 my-md-auto text-center text-md-right">
                         <h1>The Viral 2.0</h1>
@@ -31,32 +31,35 @@ function LoginLayout(props) {
                 </Row>
       </Container>
 
-      <Container>
-            <Row>
-              <Col md={6}>
-                Img
+      <Container className="py-md-5 px-4">
+            <Row className="py-5 border-bottom">
+              <Col md={"auto"} className="ml-md-4 ml-lg-5">
+                <img src="https://via.placeholder.com/150x150?text=Image" />
               </Col>
-              <Col md={6}>
+              <Col className="mr-md-4 mr-lg-5">
                <h3>Headline</h3>
                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut velit felis. Fusce consequat viverra placerat. Ut finibus metus ut sem rhoncus volutpat. Quisque mauris odio, fermentum in purus et, iaculis laoreet felis.</p>
+               <p className="mb-0"><a href="#">Sign up for an account</a></p>
               </Col>
             </Row>
-            <Row>
-                <Col md={6}>
+            <Row className="py-5 border-bottom">
+                <Col className="ml-md-4 ml-lg-5 order-last order-md-first">
                 <h3>Headline</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut velit felis. Fusce consequat viverra placerat. Ut finibus metus ut sem rhoncus volutpat. Quisque mauris odio, fermentum in purus et, iaculis laoreet felis.</p>
+                <p className="mb-0"><a href="#">Sign up for an account</a></p>
                 </Col>
-                <Col md={6}>
-                    Img
+                <Col md={"auto"} className="mr-md-4 mr-lg-5 order-first order-md-last">
+                    <img src="https://via.placeholder.com/150x150?text=Image" />
                 </Col>
             </Row>
-            <Row>
-                <Col md={6}>
-                    Img
+            <Row className="py-5">
+                <Col md={"auto"} className="ml-md-4 ml-lg-5">
+                    <img src="https://via.placeholder.com/150x150?text=Image" />
                 </Col>
-                <Col md={6}>
+                <Col className="mr-md-4 mr-lg-5">
                 <h3>Headline</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut velit felis. Fusce consequat viverra placerat. Ut finibus metus ut sem rhoncus volutpat. Quisque mauris odio, fermentum in purus et, iaculis laoreet felis.</p>
+                <p className="mb-0"><a href="#">Sign up for an account</a></p>
                 </Col>
             </Row>
       </Container>
