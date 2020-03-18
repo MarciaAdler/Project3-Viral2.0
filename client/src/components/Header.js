ab3b57e34fff6406c78a2558f627f097c3f748e4
Frank Moran <info@francisfrankmoran.com>
2020-03-18 14:12:56 -0400

    nav bar logo mobile

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Navbar, Nav } from 'react-bootstrap';
 import Logo from "../pages/assets/img/viral-effect-color.png";
+import LogoStacked from "../pages/assets/img/viral-effect-color-stacked.png";
 
 function Header(props) {
   
   return (
     <div>
-      <Navbar fixed="top" bg="light" expand="md" className="justify-content-between">
+      <Navbar fixed="top" bg="light" expand="md" className="justify-content-between py-0 py-sm-2">
         <Navbar.Brand>
-          <Nav.Link href="/home"><img src={Logo} alt="The Viral Effect" style={{width: "250px"}} /></Nav.Link>
+          <Nav.Link href="/home">
+            <img src={Logo} alt="The Viral Effect" className="d-none d-sm-inline" style={{width: "250px"}} />
+            <img src={LogoStacked} alt="The Viral Effect" className="d-inline d-sm-none" style={{width: "120px"}} />
+            </Nav.Link>
           </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end text-right">
