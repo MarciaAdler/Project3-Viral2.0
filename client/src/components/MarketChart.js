7749af209cc6f78d6307df542e6d7ca4389a03c0
Marcia <marciaadler718@gmail.com>
2020-03-19 09:47:14 -0400

    updated label for market chart

--- client/src/components/MarketChart.js
+++ client/src/components/MarketChart.js
@@ -12,7 +12,7 @@ export default function MarketChart({ stocks }) {
     labels: dates,
     datasets: [
       {
-        label: "Consumer Discretionary fund closing price",
+        label: "Consumer Discretionary fund ETF closing price",
         data: closing,
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
